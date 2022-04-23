3fde91e8aec3ce32e44258ab618666320d162823
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-04-24 06:31:03 +0800

    remove goal completion and recap into fullwidth

--- frontend/src/components/dashboardComponents/RecapReport.js
+++ frontend/src/components/dashboardComponents/RecapReport.js
@@ -53,7 +53,7 @@ const RecapReport = () => {
             {/* /.card-header */}
             <div className="card-body">
               <div className="row">
-                <div className="col-md-8">
+                <div className="col-md-12">
                   <p className="text-center">
                     <strong>Sales: 1 Jan, 2014 - 30 Jul, 2014</strong>
                   </p>
@@ -66,64 +66,6 @@ const RecapReport = () => {
                     />
                   </div>
                   {/* /.chart-responsive */}
-                </div>
-                {/* /.col */}
-                <div className="col-md-4">
-                  <p className="text-center">
-                    <strong>Goal Completion</strong>
-                  </p>
-                  <div className="progress-group">
-                    Add Products to Cart
-                    <span className="float-right">
-                      <b>160</b>/200
-                    </span>
-                    <div className="progress progress-sm">
-                      <div
-                        className="progress-bar bg-primary"
-                        style={{ width: "80%" }}
-                      />
-                    </div>
-                  </div>
-                  {/* /.progress-group */}
-                  <div className="progress-group">
-                    Complete Purchase
-                    <span className="float-right">
-                      <b>310</b>/400
-                    </span>
-                    <div className="progress progress-sm">
-                      <div
-                        className="progress-bar bg-danger"
-                        style={{ width: "75%" }}
-                      />
-                    </div>
-                  </div>
-                  {/* /.progress-group */}
-                  <div className="progress-group">
-                    <span className="progress-text">Visit Premium Page</span>
-                    <span className="float-right">
-                      <b>480</b>/800
-                    </span>
-                    <div className="progress progress-sm">
-                      <div
-                        className="progress-bar bg-success"
-                        style={{ width: "60%" }}
-                      />
-                    </div>
-                  </div>
-                  {/* /.progress-group */}
-                  <div className="progress-group">
-                    Send Inquiries
-                    <span className="float-right">
-                      <b>250</b>/500
-                    </span>
-                    <div className="progress progress-sm">
-                      <div
-                        className="progress-bar bg-warning"
-                        style={{ width: "50%" }}
-                      />
-                    </div>
-                  </div>
-                  {/* /.progress-group */}
                 </div>
                 {/* /.col */}
               </div>
