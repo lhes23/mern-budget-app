84ee68bf836d49d7ff2a44e3b316b1708497fbd8
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-05-16 20:49:41 +0800

    maessageDashboard

--- frontend/src/components/dashboardComponents/MainNavbar.js
+++ frontend/src/components/dashboardComponents/MainNavbar.js
@@ -1,17 +1,39 @@
 import MainSideBar from "./MainSideBar";
+import MessagesDashboard from "./MessagesDashboard";
+
+const DUMMY_MESSAGES = [
+  {
+    userImage: "dist/img/user1-128x128.jpg",
+    senderName: "Brad Diesel",
+    message: "Call me whenever you can...",
+    timeSent: "4 Hours Ago",
+  },
+  {
+    userImage: "dist/img/user8-128x128.jpg",
+    senderName: "John Pierce",
+    message: "I got your message bro",
+    timeSent: "4 Hours Ago",
+  },
+  {
+    userImage: "dist/img/user3-128x128.jpg",
+    senderName: "Nora Silvester",
+    message: "The subject goes here",
+    timeSent: "4 Hours Ago",
+  },
+];
 
 const MainNavbar = () => {
   return (
     <div>
       {/* Navbar */}
-      <nav className="main-header navbar navbar-expand navbar-dark">
+      <nav className="main-header navbar navbar-expand navbar-light">
         {/* Left navbar links */}
         <ul className="navbar-nav">
           <li className="nav-item">
             <a
               className="nav-link"
               data-widget="pushmenu"
-              href="#"
+              href="/#"
               role="button"
             >
               <i className="fas fa-bars" />
@@ -22,91 +44,23 @@ const MainNavbar = () => {
         <ul className="navbar-nav ml-auto">
           {/* Messages Dropdown Menu */}
           <li className="nav-item dropdown">
-            <a className="nav-link" data-toggle="dropdown" href="#">
+            <a className="nav-link" data-toggle="dropdown" href="/#">
               <i className="far fa-comments" />
               <span className="badge badge-danger navbar-badge">3</span>
             </a>
             <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-              <a href="#" className="dropdown-item">
-                {/* Message Start */}
-                <div className="media">
-                  <img
-                    src="dist/img/user1-128x128.jpg"
-                    alt="User Avatar"
-                    className="img-size-50 mr-3 img-circle"
-                  />
-                  <div className="media-body">
-                    <h3 className="dropdown-item-title">
-                      Brad Diesel
-                      <span className="float-right text-sm text-danger">
-                        <i className="fas fa-star" />
-                      </span>
-                    </h3>
-                    <p className="text-sm">Call me whenever you can...</p>
-                    <p className="text-sm text-muted">
-                      <i className="far fa-clock mr-1" /> 4 Hours Ago
-                    </p>
-                  </div>
-                </div>
-                {/* Message End */}
-              </a>
-              <div className="dropdown-divider" />
-              <a href="#" className="dropdown-item">
-                {/* Message Start */}
-                <div className="media">
-                  <img
-                    src="dist/img/user8-128x128.jpg"
-                    alt="User Avatar"
-                    className="img-size-50 img-circle mr-3"
-                  />
-                  <div className="media-body">
-                    <h3 className="dropdown-item-title">
-                      John Pierce
-                      <span className="float-right text-sm text-muted">
-                        <i className="fas fa-star" />
-                      </span>
-                    </h3>
-                    <p className="text-sm">I got your message bro</p>
-                    <p className="text-sm text-muted">
-                      <i className="far fa-clock mr-1" /> 4 Hours Ago
-                    </p>
-                  </div>
-                </div>
-                {/* Message End */}
-              </a>
-              <div className="dropdown-divider" />
-              <a href="#" className="dropdown-item">
-                {/* Message Start */}
-                <div className="media">
-                  <img
-                    src="dist/img/user3-128x128.jpg"
-                    alt="User Avatar"
-                    className="img-size-50 img-circle mr-3"
-                  />
-                  <div className="media-body">
-                    <h3 className="dropdown-item-title">
-                      Nora Silvester
-                      <span className="float-right text-sm text-warning">
-                        <i className="fas fa-star" />
-                      </span>
-                    </h3>
-                    <p className="text-sm">The subject goes here</p>
-                    <p className="text-sm text-muted">
-                      <i className="far fa-clock mr-1" /> 4 Hours Ago
-                    </p>
-                  </div>
-                </div>
-                {/* Message End */}
-              </a>
-              <div className="dropdown-divider" />
-              <a href="#" className="dropdown-item dropdown-footer">
+              {DUMMY_MESSAGES.map((msg) => {
+                return <MessagesDashboard msg={msg} />;
+              })}
+
+              <a href="/#" className="dropdown-item dropdown-footer">
                 See All Messages
               </a>
             </div>
           </li>
           {/* Notifications Dropdown Menu */}
           <li className="nav-item dropdown">
-            <a className="nav-link" data-toggle="dropdown" href="#">
+            <a className="nav-link" data-toggle="dropdown" href="/#">
               <i className="far fa-bell" />
               <span className="badge badge-warning navbar-badge">15</span>
             </a>
@@ -115,22 +69,22 @@ const MainNavbar = () => {
                 15 Notifications
               </span>
               <div className="dropdown-divider" />
-              <a href="#" className="dropdown-item">
+              <a href="/#" className="dropdown-item">
                 <i className="fas fa-envelope mr-2" /> 4 new messages
                 <span className="float-right text-muted text-sm">3 mins</span>
               </a>
               <div className="dropdown-divider" />
-              <a href="#" className="dropdown-item">
+              <a href="/#" className="dropdown-item">
                 <i className="fas fa-users mr-2" /> 8 friend requests
                 <span className="float-right text-muted text-sm">12 hours</span>
               </a>
               <div className="dropdown-divider" />
-              <a href="#" className="dropdown-item">
+              <a href="/#" className="dropdown-item">
                 <i className="fas fa-file mr-2" /> 3 new reports
                 <span className="float-right text-muted text-sm">2 days</span>
               </a>
               <div className="dropdown-divider" />
-              <a href="#" className="dropdown-item dropdown-footer">
+              <a href="/#" className="dropdown-item dropdown-footer">
                 See All Notifications
               </a>
             </div>
@@ -139,7 +93,7 @@ const MainNavbar = () => {
             <a
               className="nav-link"
               data-widget="fullscreen"
-              href="#"
+              href="/#"
               role="button"
             >
               <i className="fas fa-expand-arrows-alt" />
@@ -150,7 +104,7 @@ const MainNavbar = () => {
               className="nav-link"
               data-widget="control-sidebar"
               data-slide="true"
-              href="#"
+              href="/#"
               role="button"
             >
               <i className="fas fa-th-large" />
